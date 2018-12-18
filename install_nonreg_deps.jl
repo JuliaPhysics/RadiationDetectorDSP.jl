96eb846d6d08a001d900ea48801576522ff38653
Oliver Schulz <oschulz@mpp.mpg.de>
2018-12-18 14:29:20 +0100

    Add install_nonreg_deps.jl

---
+++ install_nonreg_deps.jl
@@ -0,0 +1,15 @@
+using Pkg
+
+curr_prj = Pkg.API.Context().env.project_file
+
+try
+    if startswith(curr_prj, pwd())
+        mv(curr_prj, curr_prj * "_orig")
+    end
+
+finally
+    if startswith(curr_prj, pwd()) && isfile(curr_prj * "_orig")
+        mv(curr_prj * "_orig", curr_prj, force = true)
+    end
+    Pkg.resolve()
+end
