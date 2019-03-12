8328863b77daecf22506fa4ff22e7bdae74293d8
Oliver Schulz <oschulz@mpp.mpg.de>
2019-03-12 14:28:17 +0100

    Add package deps

--- src/RadiationDetectorDSP.jl
+++ src/RadiationDetectorDSP.jl
@@ -4,7 +4,22 @@ __precompile__(true)
 
 module RadiationDetectorDSP
 
+using ArgCheck
+using ArraysOfArrays
+using Distributions
+using DSP
+using ElasticArrays
+using LinearAlgebra
+using ParallelProcessingTools
+using Parameters
 using RadiationDetectorSignals
+using Random
+using RecipesBase
+using StaticArrays
+using Statistics
+using StatsBase
+using TypedTables
+using UnsafeArrays
 
 # package code goes here
 
