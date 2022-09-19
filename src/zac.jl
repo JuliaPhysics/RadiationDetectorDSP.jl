c293d443cb16b47f91f35e82359083be8fed029c
Arthur Butorev <a.butorev@tum.de>
2022-09-19 17:08:14 +0200

    add function zac chained with differential filter

--- src/zac.jl
+++ src/zac.jl
@@ -21,17 +21,19 @@ ZAC filter function as defined in Eq. (7) in [Eur. Phys. J. C (2015) 75:255]
 end
 
 @inline zac_diff_filter_coefficients(t, Δt, τ, τₛ, L, FT) = begin
-    a = exp(-1/τ)
+    a = exp(-Δt/τ)
     if 0 <= t < L
         A = _A(L, FT, τₛ)
         # @debug "A = $A"
-        sinh((t+Δt)/τₛ) - a*sinh(t/τₛ) + A*((t + Δt)*Δt + (t + Δt - a*t)*(t - L))
+        sinh((t+Δt)/τₛ) - a*sinh(t/τₛ) 
+        + A*((t + Δt)*Δt + (t + Δt - a*t)*(t - L))
     elseif L <= t <= L+FT
         (1-a)*sinh(L/τₛ)
-    elseif L + FT < t <= 2L + FT
+    elseif L + FT < t <= zd2L + FT
         A = _A(L, FT, τₛ)
         # @debug "A = $A"
-        -a*(sinh((t+Δt)/τₛ) - sinh(t/τₛ)/a + A*((t + Δt)*Δt + (t + Δt - t/a)*(t - L)))
+        t̃ = 2L + FT - t
+        -a*(sinh((t̃+Δt)/τₛ) - sinh(t̃/τₛ)/a + A*((t̃ + Δt)*Δt + (t̃ + Δt - t̃/a)*(t̃ - L)))
     else
         0
     end
@@ -71,45 +73,13 @@ function create_zac_filter(Nₜ, FTₜ, τₜ, Δt)
         CUSP[L+i] = C
     end
 
-    ∫Poly = sum(Poly)
-    ∫CUSP = sum(CUSP)
+    ∫Poly = mean(Poly)
+    ∫CUSP = mean(CUSP)
     @debug "∫Poly = $(∫Poly)"
     @debug "∫CUSP = $(∫CUSP)"
     @debug "A = $(-∫CUSP/∫Poly)"
-    # build the normalized ZAC filter
-    CUSP .+ (Poly .* (-∫CUSP/∫Poly))
-end
+    @debug "check normalization: $(sum(CUSP .+ (Poly .* (-∫CUSP/∫Poly))))"
 
-function create_zac_diff_filter(Nₜ, FTₜ, τₜ, τ₂ₜ, Δt)
-    # convert parameters to integers respecting whatever resolution is 
-    # given by Δt
-    N, τₛ, FT, τ = tons.((Nₜ, τₜ, FTₜ, τ₂ₜ), Δt)
-
-    # define length of filter
-    L = ((N - FT) % 2 == 0) ? (N - FT)÷2 : (N - (FT+=1))÷2
-    @debug "partial length: $L"
-    @debug "Total length of Filter: $N"
-    @debug "exponential decay: $(τₛ)"
-    @debug "Flat top: $FT"
-    CUSP = Array{Float64, 1}(undef, N)
-    Poly = zeros(Int, N)
-
-    # build the cusp filter (sinh part) and polynomial part
-    for i in 1:L
-        CUSP[i] = CUSP[N-i+1] = sinh(i/τₛ)
-        Poly[i] = Poly[N-i+1] = i*(i - L)
-    end
-    # build the flat top part
-    C = sinh(L/τₛ)
-    for i in 1:FT
-        CUSP[L+i] = C
-    end
-
-    ∫Poly = sum(Poly)
-    ∫CUSP = sum(CUSP)
-    @debug "∫Poly = $(∫Poly)"
-    @debug "∫CUSP = $(∫CUSP)"
-    @debug "A = $(-∫CUSP/∫Poly)"
     # build the normalized ZAC filter
     CUSP .+ (Poly .* (-∫CUSP/∫Poly))
 end
