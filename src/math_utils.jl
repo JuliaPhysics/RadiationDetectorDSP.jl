f151f24334957e6db22ad8e4789b8e55b3b060b4
Oliver Schulz <oschulz@mpp.mpg.de>
2023-03-23 15:14:07 +0100

    Add _floattype

--- src/math_utils.jl
+++ src/math_utils.jl
@@ -1,6 +1,15 @@
 # This file is a part of RadiationDetectorDSP.jl, licensed under the MIT License (MIT).
 
 
+_floattype(::Type{T}) where T = float(T)
+_floattype(::Type{Int8}) = Float32
+_floattype(::Type{UInt8}) = Float32
+_floattype(::Type{Int16}) = Float32
+_floattype(::Type{UInt16}) = Float32
+_floattype(::Type{Int32}) = Float32
+_floattype(::Type{UInt32}) = Float32
+
+
 function _smoothstep(x::Real)
     @fastmath begin
         xc = ifelse(x < zero(x), zero(x), ifelse(x > one(x), one(x), x))
