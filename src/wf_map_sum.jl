6fb2aedddb8ac54e82905d4cd307cd6acb1451ab
Oliver Schulz <oschulz@mpp.mpg.de>
2023-05-13 10:36:24 +0200

    STASH wf_map_cum seems correct

--- src/wf_map_sum.jl
+++ src/wf_map_sum.jl
@@ -9,11 +9,15 @@ _acc_weighted_add(acc, x, weight::Number) = muladd(weight, x, acc)
 _acc_weighted_add(acc::NTuple{N}, x::NTuple{N}, weight::Number) where N = ntuple(i -> _acc_weighted_add(acc[i], x[i], weight), Val(N))
 _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) where names = NamedTuple{names}(_acc_weighted_add(values(acc), values(x), weight))
 
+_deep_mul(x, weight::Number) = weight * x
+_deep_mul(x::NTuple{N}, weight::Number) where N = ntuple(i -> _deep_mul(x[i], weight), Val(N))
+_deep_mul(x::NamedTuple{names}, weight::Number) where names = NamedTuple{names}(_deep_mul(values(x), weight))
+
 
 @kernel function wf_map_sum_kernel_impl(
     f_presum,
     f_postsum,
-    Y::AbstractVector{<:Real},
+    Y::AbstractVector,
     X::AbstractMatrix{<:Real},
     I_start::Union{Real,AbstractVector{<:Real}},
     n::Integer
@@ -29,10 +33,12 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
     @uniform tile_size = n_workers
     @uniform n_eff = n + 1
     @uniform n_tiles = div(n_eff + n_workers - 1, n_workers)
-    @uniform weight_one = one(promote_type(eltype(Y),eltype(X)))
-    @uniform acc_initval = f_presum(-zero(T_in)) * weight_one
+    @uniform weight_one = one(_floattype(eltype(X)))
 
-    #worker == 1 && worker_group ==1 && @info "DEBUG" tile_size n_tiles
+    # Assumes that f_presum(one(T_in) is not NaN or Inf:
+    @uniform acc_initval = _deep_mul(f_presum(one(T_in)), zero(weight_one))
+
+    #worker == 1 && worker_group ==1 && @debug "wf_map_sum_kernel_impl:" tile_size n_tiles
 
     buf = @localmem T_in (tile_size, tile_size)
 
@@ -63,7 +69,7 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
                     i_start = floor(Int, i_start_real)
                     i_start_offset = i_start - first(axes(X,1))
                     global_i = global_i_base + i_start_offset
-                    #@info "DEBUG $((;local_j, global_j, local_i, global_i))"
+                    #@debug "wf_map_sum_kernel_impl buffering: $((;local_j, global_j, local_i, global_i))"
                     if global_i in axes(X,1)
                         buf[local_j, local_i] = convert(T_in, X[global_i, global_j])
                     end
@@ -74,7 +80,7 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
         @synchronize
 
         let
-            #worker == 1 && @info "DEBUG" buf
+            #worker == 1 && @debug "wf_map_sum_kernel_impl" buf
 
             global_j_offset = (worker_group-1) * tile_size + first(axes(X,2)) - 1
             local_j = worker
@@ -88,10 +94,10 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
                 weight_last = i_start_real - i_start
                 weight_first = weight_one - weight_last
 
-                #@info "DEBUG $((;tile, tile_size, n_eff, n_i, i_start, weight_last))"
+                #@debug "wf_map_sum_kernel_impl: $((;tile, tile_size, n_eff, n_i, i_start, weight_last))"
                 if n_i > 1
                     tmp_acc = let local_i = 1
-                        #@info "DEBUG $((;local_j, local_i))"
+                        #@debug "wf_map_sum_kernel_impl summing: $((;local_j, local_i))"
                         f_x = f_presum(buf[local_j, local_i])
                         weight = ifelse(tile == 1, weight_first, weight_one)
                         _acc_weighted_add(tmp_acc, f_x, weight)
@@ -99,17 +105,17 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
                 end
                 #!!!@fastmath @inbounds @simd
                 for local_i in 2:(n_i - 1)
-                    #@info "DEBUG $((;local_j, local_i))"
+                    #@debug "wf_map_sum_kernel_impl summing: $((;local_j, local_i))"
                     f_x = f_presum(buf[local_j, local_i])
                     tmp_acc = _acc_add(tmp_acc, f_x)
                 end
                 tmp_acc = let local_i = n_i
-                    #@info "DEBUG $((;local_j, local_i))"
+                    #@debug "wf_map_sum_kernel_impl summing: $((;local_j, local_i))"
                     f_x = f_presum(buf[local_j, local_i])
                     weight = ifelse(tile == n_tiles, weight_last, weight_one)
                     _acc_weighted_add(tmp_acc, f_x, weight)
                 end
-                acc[1] += tmp_acc
+                acc[1] = _acc_add(acc[1], tmp_acc)
             end
         end
     end
@@ -122,4 +128,24 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
     #!!!@inbounds
     Y[global_j] = f_postsum(acc[1])
     end
+end
+
+
+function run_wf_map_sum_kernel!(
+    f_presum,
+    f_postsum,
+    Y::AbstractVector,
+    X::AbstractMatrix{<:Real},
+    I_start::Union{Real,AbstractVector{<:Real}},
+    n::Integer
+)
+    backend = _ka_get_backend(X)
+    n_workers = 32
+    kernel! = wf_map_sum_kernel_impl(backend, (n_workers,))
+    n_waveforms = length(axes(X, 2))
+    n_tiles = div(n_waveforms + n_workers - 1, n_workers)
+    div(n + n_workers - 1, n_workers)
+    kernel_ret = kernel!(f_presum, f_postsum, Y, X, I_start, n, ndrange=(n_tiles * n_workers,)) 
+    _ka_synchronize(kernel!, kernel_ret)
+    return Y
 end
