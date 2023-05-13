d4ad9d6c769b3bbd66799a13796bcc17c6992459
Oliver Schulz <oschulz@mpp.mpg.de>
2023-05-13 10:36:24 +0200

    STASH

--- src/wf_map_sum.jl
+++ src/wf_map_sum.jl
@@ -33,6 +33,8 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
     @uniform w_last = w_one - w_first
     @uniform acc_initval = f_presum(-zero(T_in)) * w_one
 
+    worker == 1 && worker_group ==1 && @info "DEBUG" tile_size n_tiles
+
     buf = @localmem T_in (tile_size, tile_size)
 
     acc = @private typeof(acc_initval) 1
@@ -47,43 +49,66 @@ _acc_weighted_add(acc::NamedTuple{names}, x::NamedTuple{names}, weight::Number) 
         global_j_offset = (worker_group-1) * tile_size + first(axes(X,2)) - 1
         local_i = worker
         global_i_base = global_i_offset + local_i
+
+        #!!!@fastmath @inbounds @simd
+        for local_j in Base.OneTo(tile_size)
+            buf[local_j, local_i] = nan_value
+        end
+
         #!!!@fastmath @inbounds @simd
         for local_j in Base.OneTo(tile_size)
             global_j = global_j_offset + local_j
-            i_start_offset = _kbc_getindex(I_start, global_j) - first(axes(X,1))
-            global_i = global_i_base + i_start_offset
-            if global_i in axes(X,1) && global_j in axes(X,2)
-                buf[local_j, local_i] = convert(T_in, X[global_i, global_j])
-            else
-                buf[local_j, local_i] = nan_value
+            if global_j in axes(X,2)
+                i_start_offset = _kbc_getindex(I_start, global_j) - first(axes(X,1))
+                global_i = global_i_base + i_start_offset
+                #@info "DEBUG $((;local_j, global_j, local_i, global_i))"
+                if global_i in axes(X,1)
+                    buf[local_j, local_i] = convert(T_in, X[global_i, global_j])
+                end
             end
         end
 
         @synchronize
 
+        #worker == 1 && @info "DEBUG" buf
+
+        global_j_offset = (worker_group-1) * tile_size + first(axes(X,2)) - 1
+        local_j = worker
+        global_j = global_j_offset + local_j
+        n_i = min(n - (tile-1) * tile_size, tile_size)
         tmp_acc::typeof(acc_initval) = acc_initval
-        tmp_acc = let i = 1
-            f_x = f_presum(buf[worker, i])
-            weight = ifelse(tile == 1, w_first, w_one)
-            _acc_weighted_add(tmp_acc, f_x, weight)
+        if global_j in axes(X,2)
+            #@info "DEBUG $((;tile, tile_size, n, n_i))"
+            if n_i > 1
+                tmp_acc = let local_i = 1
+                    #@info "DEBUG $((;local_j, local_i))"
+                    f_x = f_presum(buf[local_j, local_i])
+                    weight = ifelse(tile == 1, w_first, w_one)
+                    _acc_weighted_add(tmp_acc, f_x, weight)
+                end
+            end
+            #!!!@fastmath @inbounds @simd
+            for local_i in 2:(n_i - 1)
+                #@info "DEBUG $((;local_j, local_i))"
+                f_x = f_presum(buf[local_j, local_i])
+                tmp_acc = _acc_add(tmp_acc, f_x)
+            end
+            tmp_acc = let local_i = n_i
+                #@info "DEBUG $((;local_j, local_i))"
+                f_x = f_presum(buf[local_j, local_i])
+                weight = ifelse(tile == n_tiles, w_last, w_one)
+                _acc_weighted_add(tmp_acc, f_x, weight)
+            end
+            acc[1] += tmp_acc
         end
-        #!!!@fastmath @inbounds @simd
-        for i in 2:(tile_size - 1)
-            f_x = f_presum(buf[worker, i])
-            tmp_acc = _acc_add(tmp_acc, f_x)
-        end
-        tmp_acc = let i = tile_size
-            f_x = f_presum(buf[worker, i])
-            weight = ifelse(tile == n_tiles, w_last, w_one)
-            _acc_weighted_add(tmp_acc, f_x, weight)
-        end
-        acc[1] += tmp_acc
     end
 
     # Sanity check:
     global_j, = @index(Global, NTuple)
     @assert global_j == (worker_group-1) * tile_size + first(axes(X,2)) - 1 + worker
 
+    if global_j in axes(X,2)
     #!!!@inbounds
     Y[global_j] = f_postsum(acc[1])
+    end
 end
