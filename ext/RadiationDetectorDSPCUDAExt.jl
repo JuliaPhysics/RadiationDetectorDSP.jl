1da6c64a89c7af849c7924e5da09a2c04e399000
Oliver Schulz <oschulz@mpp.mpg.de>
2026-04-16 10:05:10 +0200

    Remove _nonlazy_transpose specialization for CuArray
    CUDA.jl natively specializes transpose! now.

--- ext/RadiationDetectorDSPCUDAExt.jl
+++ ext/RadiationDetectorDSPCUDAExt.jl
@@ -7,14 +7,6 @@ using RadiationDetectorDSP
 import Adapt
 
 
-function RadiationDetectorDSP._nonlazy_transpose(A::CuArray{T,2}) where {T<:Number}
-    A_T = similar(A, reverse(size(A)))
-    # CUBLAS.geam! is extremely fast:
-    CUDA.CUBLAS.geam!('T', 'T', one(T), A, zero(T), A, A_T)
-    return A_T
-end
-
-
 RadiationDetectorDSP.deviceof_impl(@nospecialize(TypeHistory::Type), A::CuArray) = CUDA.device(A)
 RadiationDetectorDSP.deviceof_impl(@nospecialize(TypeHistory::Type), A::CUDA.CUDA.CUSPARSE.AbstractCuSparseArray) = CUDA.device(A.nzVal)
 
